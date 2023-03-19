50f1d795cf9ec548cb1537c67546fe710fb18904
Rownokzahan <84782117+Rownokzahan@users.noreply.github.com>
2023-03-19 17:15:49 +0600

    Seperate view from logical part

---
+++ edit.view.php
@@ -0,0 +1,40 @@
+<?php include_once __DIR__ . "./partials/header.php" ?>
+
+<h5 class="text-3xl font-semibold text-center">Update Task</h5>
+<!-- Update Form -->
+<form action="edit.php" method="POST" enctype="multipart/form-data" id="update-form">
+    <input type="hidden" value="<?= $task->id; ?>" name="id">
+    <div class="form-control w-full mb-4">
+        <label class="label">
+            <span class="label-text">Name</span>
+        </label>
+        <input type="text" name="name" value="<?= $task->name; ?>" placeholder="Task Name" class="input input-bordered w-full" />
+    </div>
+    <div class="form-control w-full mb-4">
+        <label class="label">
+            <span class="label-text">Deadline</span>
+        </label>
+        <input type="date" value="<?= $task->deadline; ?>" name="deadline" class="input input-bordered w-full" />
+    </div>
+    <div class="form-control w-full mb-4">
+        <label class="label">
+            <span class="label-text">Status</span>
+        </label>
+        <select name="status" class="select select-bordered flex-1">
+            <option disabled>Status</option>
+            <option <?php if ($task->status == 'In progress') echo ' selected'; ?>>In Progress</option>
+            <option <?php if ($task->status == 'Complete') echo ' selected'; ?>>Complete</option>
+            <option <?php if ($task->status == 'Incomplete') echo ' selected'; ?>>Incomplete</option>
+        </select>
+    </div>
+    <div class="form-control w-full mb-4">
+        <label class="label">
+            <span class="label-text">Attachment</span>
+        </label>
+        <input type="file" accept="image/*" name="image" class="file-input file-input-bordered w-full" required />
+    </div>
+    <button type="submit" class="bg-[#8e40ee] px-5 py-[6px] text-white font-semibold rounded shadow-md block ml-auto">Update</button>
+</form>
+
+
+<?php include_once __DIR__ . "./partials/footer.php" ?>
--- index.php
+++ index.php
@@ -1,63 +1,7 @@
-<?php include_once __DIR__ . "./partials/header.php" ?>
+<?php
+require_once __DIR__ . "./connect.php";
+$query = "SELECT * FROM tasks ORDER BY id";
+$statement = $pdo->query($query);
+$tasks = $statement->fetchAll(PDO::FETCH_OBJ);
 
-<div class="flex justify-between items-center pb-8">
-    <h1 class="text-center text-2xl font-semibold uppercase text-white">To Do List</h1>
-    <a href="./create.php" class="h-fit bg-[#8e40ee] px-5 py-[6px] text-white font-semibold rounded-xl shadow-md">New Task</a>
-</div>
-<div class="overflow-x-auto">
-    <table class="table w-full">
-        <thead>
-            <tr>
-                <th>SL</th>
-                <th>Name</th>
-                <th>Attachment</th>
-                <th>Start Time</th>
-                <th>Deadline</th>
-                <th>Status</th>
-                <th>Edit</th>
-                <th>Delete</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-            require_once __DIR__ . "./connect.php";
-            $query = "SELECT * FROM tasks ORDER BY id";
-            $statement = $pdo->query($query);
-            $tasks = $statement->fetchAll(PDO::FETCH_OBJ);
-            $count = 1;
-            foreach ($tasks as $task) :
-            ?>
-                <tr class="hover">
-                    <th>
-                        <?php echo $count; ?>
-                    </th>
-                    <td>
-                        <?php echo $task->name; ?>
-                    </td>
-                    <td><img src="<?php echo $task->attachment; ?>" class="w-12 h-12 rounded" alt=""></td>
-                    <td>
-                        <?php echo $task->start_time; ?>
-                    </td>
-                    <td>
-                        <?php echo $task->deadline; ?>
-                    </td>
-                    <td>
-                        <?php echo $task->status; ?>
-                    </td>
-                    <td>
-                        <a href="edit.php?id=<?= $task->id; ?>" class="bi bi-pencil-square mr-8 text-[#8e40ee] text-xl"></a>
-                    </td>
-                    <td>
-                        <a href="delete.php?id=<?= $task->id; ?>" class="bi bi bi-trash mr-4 text-red-500 text-xl"></a>
-                    </td>
-                </tr>
-            <?php
-                $count++;
-            endforeach;
-            ?>
-        </tbody>
-    </table>
-</div>
-
-
-<?php include_once __DIR__ . "./partials/footer.php" ?>
+require_once __DIR__ . "./index.view.php";
